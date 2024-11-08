32ca1562a5c99c6dfb4f760217c5573e9f3949b1
ppsmoraes <ppsmoraes@gmail.com>
2024-11-07 21:13:40 -0300

    Add Game.play()

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 class Game:
     def __init__(self, size: int):
         self.size: int = size
+
+    def set_size(self, new_size: int):
+        self.size: int = new_size
     
     def start(self) -> None:
         # Valores a serem mostrado no jogo
@@ -23,8 +26,28 @@ class Game:
         self.side_sums: list[int] = [int(sum(row)) for row in self.answers_values]
         self.top_sums: list[int] = [int(sum([self.answers_values[i][col] for i in range(self.size)])) for col in range(self.size)]
 
-    def change_size_to(self, new_size: int):
-        self.size: int = new_size
+    def play(self) -> None:
+        row: int = int(input('Selecione uma linha: '))
+        col: int = int(input('Selecione uma coluna: '))
+        action: str = input('Deseja apagar ou selecionar? ')
+
+        match action.lower():
+            case 'apagar':
+                print(f'O usuário deseja apagar o valor {self.values[row][col]}.')
+                if not self.answers[row][col]:
+                    print('Valor apagado com sucesso!')
+                else :
+                    print('Não foi dessa vez.')
+            case 'selecionar':
+                print(f'O usuário deseja selecionar o valor {self.values[row][col]}.')
+                if self.answers[row][col]:
+                    print('Valor selecionado com sucesso!')
+                else :
+                    print('Não foi dessa vez.')
+            case _:
+                pass
+
+
 
 def test() -> None:
     x: Game = Game(5)
@@ -32,6 +55,7 @@ def test() -> None:
     print(f': {x.top_sums}')
     for i, line in enumerate(x.values):
         print(f'{x.side_sums[i]}: {line}')
+    x.play()
         
 
 def main() -> None:
